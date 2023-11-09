6a0bd0bd663423b442514f4f7f993465be30a711
Grant <284885069@qq.com>
2023-11-09 22:50:26 +0800

    feat: 本地环境变量，脚本配置

--- packages/server/src/config/app.ts
+++ packages/server/src/config/app.ts
@@ -2,6 +2,7 @@ import { ConfigModule } from '@nestjs/config';
 import {NacosManager} from "./nacos";
 
 export default ConfigModule.forRoot({
+    envFilePath: ['.env', '.env.local'],
     load: [async () => {
         const configManager = new NacosManager();
         return await configManager.getAllConfig();
