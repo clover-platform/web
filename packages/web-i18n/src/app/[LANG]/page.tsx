0128aa419b3279bf0ce56b1455a10a806a729186
Grant <284885069@qq.com>
2024-04-06 21:37:39 +0800

    feat: 404 和 ci 脚本

---
+++ packages/web-i18n/src/app/[LANG]/page.tsx
@@ -0,0 +1,10 @@
+import {keywords, title} from "@clover/public/utils/seo";
+
+export const metadata = {
+    title: title("{#首页#}"),
+    keywords: keywords(),
+}
+
+const Page = () => <></>
+
+export default Page;
