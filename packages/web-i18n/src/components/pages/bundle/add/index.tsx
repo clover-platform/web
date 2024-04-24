f1cbc3b3b228a2a433d3262998e5cc2a6bca8baa
Grant <284885069@qq.com>
2024-04-24 18:22:17 +0800

    feat: add bundle

--- packages/web-i18n/src/components/pages/bundle/add/index.tsx
+++ packages/web-i18n/src/components/pages/bundle/add/index.tsx
@@ -1,15 +1,30 @@
 'use client';
 
 import {TitleBar} from "@clover/public/components/common/title-bar";
-import {Button, Space} from "@atom-ui/core";
+import { Button, Space, useMessage } from "@atom-ui/core";
 import BackButton from "@easy-kit/common/components/button/back";
 import {useState} from "react";
 import {BundleForm} from "@/components/pages/bundle/form";
+import { create, AddBundleData } from "@/rest/bundle";
+import { useRouter, useSearchParams } from "next/navigation";
 
 export const AddBundlePage = () => {
+    const search = useSearchParams();
+    const id = search.get("id");
     const [loading, setLoading] = useState(false);
-    const onSubmit = async (data: any) => {
-        console.log(data);
+    const msg = useMessage();
+    const router = useRouter();
+
+    const onSubmit = async (data: AddBundleData) => {
+        setLoading(true);
+        data.moduleId = Number(id);
+        const { success, message } = await create(data);
+        setLoading(false);
+        if(success) {
+            router.push("/{#LANG#}/i18n/bundle/?id=" + id);
+        }else{
+            msg.error(message);
+        }
     }
 
     return <>
--- packages/web-i18n/src/rest/bundle.ts
+++ packages/web-i18n/src/rest/bundle.ts
@@ -1,5 +1,5 @@
 import {RestResult} from "@easy-kit/common/types/rest";
-import {get} from "@easy-kit/common/utils/rest";
+import {get, post} from "@easy-kit/common/utils/rest";
 import {Bundle} from "@/types/pages/bundle";
 
 export type BundleQuery = {
@@ -10,3 +10,19 @@ export type BundleQuery = {
 
 export const list = async (query: BundleQuery): Promise<RestResult<Bundle>> =>
     get(`@i18n/bundle/list/`, query);
+
+export type AddBundleDataExport = {
+    config: any;
+    format: string;
+}
+
+export type AddBundleData = {
+    name: string;
+    sources: string[];
+    export: AddBundleDataExport;
+    includeSource: boolean;
+    moduleId: number;
+}
+
+export const create = async (data: AddBundleData): Promise<RestResult<any>> =>
+    post(`@i18n/bundle/create/`, data);
