97071d46f4edfbc5dee32f2214a5a513858fdcbd
Grant <284885069@qq.com>
2024-10-11 19:12:35 +0800

    feat: 打包优化

--- apps/main/tailwind.config.ts
+++ apps/main/tailwind.config.ts
@@ -1,11 +1,16 @@
 import type {Config} from 'tailwindcss';
-import coreConfig, {commonContent} from "@easykit/design/lib/tailwind";
+import {tailwindConfig } from "@easykit/design/lib";
 import ta from "tailwindcss-animate";
 import plugin from "@easy-kit/common/plugin/tailwind";
 
 const config: Config = {
-    ...coreConfig,
-    content: commonContent,
+    ...tailwindConfig,
+    content: [
+        "../../packages/core/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "../../packages/common/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "../public/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/**/*.{js,ts,jsx,tsx,mdx}",
+    ],
     plugins: [
         ta,
         plugin
