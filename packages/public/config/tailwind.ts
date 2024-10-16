6e2d340e6873d07fb2e022eca507a511f317f28a
Grant <284885069@qq.com>
2024-10-16 16:22:20 +0800

    feat: clear packages and fix tailwind content

--- packages/public/config/tailwind.ts
+++ packages/public/config/tailwind.ts
@@ -1,6 +1,6 @@
 export const content = [
     "../../packages/core/components/**/*.{js,ts,jsx,tsx,mdx}",
     "../../packages/common/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "../public/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "../../packages/public/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/**/*.{js,ts,jsx,tsx,mdx}",
 ]
