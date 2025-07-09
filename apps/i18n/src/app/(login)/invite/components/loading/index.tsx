1fc11431ea16bb267a2f9dbab1fa09a743988a2d
Grant <284885069@qq.com>
2025-07-10 01:22:00 +0800

    style: 加入项目

--- apps/i18n/src/app/(login)/invite/components/loading/index.tsx
+++ apps/i18n/src/app/(login)/invite/components/loading/index.tsx
@@ -32,7 +32,7 @@ export const InvitePageLoading = () => {
                   <Skeleton className="h-4 w-16" />
                 </div>
               </div>
-              <div className="-mx-2 h-0.5 bg-white" />
+              <div className="-mx-2 h-0.5 bg-white dark:bg-black" />
               <div className="flex items-center justify-center">
                 <div className="mr-2">
                   <Skeleton className="h-6 w-8" />
