e4d0b434554fcf3ea46ee660b3f6e05f55a9ab76
Grant <284885069@qq.com>
2025-08-06 18:37:00 +0800

    feat: update biome

--- apps/main/src/components/layout/main/index.tsx
+++ apps/main/src/components/layout/main/index.tsx
@@ -1,39 +1,40 @@
-import {useLayoutProps} from "@clover/public/components/layout/hooks/use.layout.props";
+import { Search } from './search'
+import { Sidebar } from './sidebar'
+
+import type { FC } from 'react'
+import { useTranslation } from 'react-i18next'
+import { useLayoutProps } from '@clover/public/components/layout/hooks/use.layout.props'
 import {
   MainLayout as PublicMainLayout,
   type MainLayoutProps as PublicMainLayoutProps,
 } from '@clover/public/components/layout/main'
-import {useUnauthorizedHandle} from "@clover/public/hooks/use.unauthorized.handle";
-import type { FC } from 'react'
-import { useTranslation } from 'react-i18next'
-import { Search } from './search'
-import { Sidebar } from './sidebar'
+import { useUnauthorizedHandle } from '@clover/public/hooks/use.unauthorized.handle'
 
 export type MainLayoutProps = PublicMainLayoutProps & {
-  active?: string;
-};
+  active?: string
+}
 
 export const MainLayout: FC<MainLayoutProps> = (origin) => {
-  const props = useLayoutProps<MainLayoutProps>(origin);
+  const props = useLayoutProps<MainLayoutProps>(origin)
   const { active } = props
   const { t } = useTranslation()
   const { isLogin } = useUnauthorizedHandle()
 
   return (
     <PublicMainLayout
+      bodyClassName="flex w-full"
       className="bg-secondary dark:bg-background"
+      container={false}
+      footerProps={{
+        enable: false,
+      }}
       headerProps={{
         logoUrl: '/',
         appName: t('项目'),
         className: 'bg-background dark:bg-black/50',
         extra: <Search />,
       }}
-      footerProps={{
-        enable: false,
-      }}
       loading={!isLogin}
-      container={false}
-      bodyClassName="flex w-full"
     >
       <div className="h-[calc(100vh-var(--layout-header-height))] w-72 border-border border-r bg-background dark:bg-black/50">
         <Sidebar active={active} />
